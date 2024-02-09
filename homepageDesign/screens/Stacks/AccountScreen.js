18fecb1d70b4ad9b640403e75cc11abc229cd1fd
harshit-506 <supriyamohapatra658@gmail.com>
2024-02-09 16:06:32 +0530

    done with account Screen

--- homepageDesign/screens/Stacks/AccountScreen.js
+++ homepageDesign/screens/Stacks/AccountScreen.js
@@ -1,21 +1,134 @@
-import React, { memo, useCallback } from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { AntDesign, MaterialCommunityIcons, Feather } from '@expo/vector-icons';
+import React, { memo, useCallback, useContext } from 'react';
+import { Pressable, SafeAreaView, StyleSheet, Text, View, ScrollView } from 'react-native';
+import { AntDesign, MaterialCommunityIcons, Feather, FontAwesome, SimpleLineIcons, Entypo, Ionicons, Octicons, EvilIcons,FontAwesome5,MaterialIcons } from '@expo/vector-icons';
+import { UserType } from '../../context/contextApi';
 
 const AccountScreen = () => {
+  const {authenticated}=useContext(UserType);
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <Text style={[styles.headerText, styles.boldText]}>+ Phone Number</Text>
-        <Text style={[styles.headerText, styles.boldText]}>Explore + Premium</Text>
-      </View>
-      <View style={styles.line} />
-      <View style={styles.menuContainer}>
-        <MenuItem icon="CodeSandbox" text="Order" />
-        <MenuItem icon="hearto" text="Wishlist" />
-        <MenuItem icon="offer" text="Coupons" />
-        <MenuItem icon="headphones" text="Help Center" />
-      </View>
+      <ScrollView contentContainerStyle={styles.scrollViewContent} showsVerticalScrollIndicator={false}>
+        {authenticated ? (
+        
+          
+          <>
+          <View style={styles.header}>
+          <Text style={[styles.headerText, styles.boldText]}>+ Phone Number</Text>
+          <Text style={[styles.headerText, styles.boldText]}>Explore + Premium</Text>
+        </View>
+        <View style={styles.line} />
+        <View style={styles.menuContainer}>
+          <MenuItem icon="CodeSandbox" text="Order" />
+          <MenuItem icon="hearto" text="Wishlist" />
+          <MenuItem icon="offer" text="Coupons" />
+          <MenuItem icon="headphones" text="Help Center" />
+        </View>
+        </>
+        ):(
+          
+          <View style={styles.verifyContainer1}>
+          <View style={styles.accountSettingHeader}>
+            <Text style={styles.accountText1}>Account</Text>
+          </View>
+          <Text style={styles.verifyText1}>Log in for exclusive offers</Text>
+          <Pressable style={styles.verifyButton1}>
+            <Text style={styles.verifyButtonText1}>Log In</Text>
+          </Pressable>
+        </View>
+        )}
+       
+        {/* for verifying email */}
+        <View style={styles.verifyContainer}>
+          <Text style={styles.verifyText}>Verify your email</Text>
+          <Pressable>
+            <Text style={styles.verifyButton}>Update</Text>
+          </Pressable>
+        </View>
+        {/* Account Setting */}
+        <View style={styles.accountSection}>
+          <Text style={styles.accountText}>Account Setting</Text>
+          <View style={styles.accountItem}>
+            <FontAwesome name="plus-square-o" size={20} color="gray" />
+            <Text style={styles.accountItemText}>HarryCart Plus</Text>
+          </View>
+          <View style={styles.accountItem}>
+            <MaterialCommunityIcons name="account-circle-outline" size={20} color="black" />
+            <Text style={styles.accountItemText}>Edit Profile</Text>
+          </View>
+          <View style={styles.accountItem}>
+            <MaterialCommunityIcons name="wallet-outline" size={20} color="black" />
+            <Text style={styles.accountItemText}>Saved Wallet</Text>
+          </View>
+          <View style={styles.accountItem}>
+            <SimpleLineIcons name="location-pin" size={20} color="black" />
+            <Text style={styles.accountItemText}>Saved Address</Text>
+          </View>
+          <View style={styles.accountItem}>
+            <Entypo name="language" size={19} color="black" />
+            <Text style={styles.accountItemText}>Select Language</Text>
+          </View>
+          <View style={styles.accountItem}>
+            <Ionicons name="notifications-outline" size={20} color="black" />
+            <Text style={styles.accountItemText}>Notification Setting</Text>
+          </View>
+        </View>
+        {/* my Activity section */}
+        <View style={styles.accountSection}>
+          <Text style={styles.accountText}>My Activity</Text>
+          <View>
+            <View style={styles.accountItem}>
+              <Octicons name="code-review" size={20} color="black" />
+              <Text style={styles.accountItemText}>Reviews</Text>
+            </View>
+            <View style={styles.accountItem}>
+              <EvilIcons name="question" size={20} color="black" />
+              <Text style={styles.accountItemText}>Questions and Answers</Text>
+            </View>
+          </View>
+        </View>
+
+        {/* Earning section */}
+        <View style={styles.accountSection}>
+          <Text style={styles.accountText}>Earn with HarryCart</Text>
+          <View>
+            <View style={styles.accountItem}>
+            <FontAwesome5 name="tiktok" size={19} color="black" />
+              <Text style={styles.accountItemText}>HarryCart creator Studio</Text>
+            </View>
+            <View style={styles.accountItem}>
+            <FontAwesome name="dollar" size={20} color="black" />
+              <Text style={styles.accountItemText}>Sell on HarryCart</Text>
+            </View>
+          </View>
+        </View>
+
+        {/* feedback and information */}
+        <View style={styles.accountSection}>
+          <Text style={styles.accountText}>Feedback & Information</Text>
+          <View>
+            <View style={styles.accountItem}>
+            <Entypo name="documents" size={20} color="black" />
+              <Text style={styles.accountItemText}>Terms,Policies and Licences</Text>
+            </View>
+            <View style={styles.accountItem}>
+            <MaterialIcons name="question-answer" size={20} color="black" />
+              <Text style={styles.accountItemText}>Browse FAQs</Text>
+            </View>
+          </View>
+        </View>
+
+         {/* Logout button */}
+         {authenticated ? (
+          <View style={styles.logoutContainer}>
+          <Pressable style={styles.logoutButton}>
+            <Text style={styles.logoutButtonText}>Logout</Text>
+          </Pressable>
+        </View>
+         ):(
+null
+         )}
+        
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -57,6 +170,10 @@ const styles = StyleSheet.create({
     paddingTop: 40,
     paddingHorizontal: 20,
   },
+  scrollViewContent: {
+    flexGrow: 1,
+    paddingBottom: 20,
+  },
   header: {
     marginBottom: 20,
   },
@@ -82,7 +199,7 @@ const styles = StyleSheet.create({
   menuItem: {
     width: '48%',
     flexDirection: 'row',
-    paddingLeft:35,
+    paddingLeft: 35,
     alignItems: 'center',
     borderWidth: 1,
     borderColor: '#ccc',
@@ -104,6 +221,160 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#333',
     marginLeft: 10,
+  },
+  verifyContainer: {
+    flexDirection: 'row',
+    paddingLeft: 12,
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingVertical: 10,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    marginBottom: 10,
+  },
+  verifyText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginRight: 7,
+    color: '#333',
+    marginRight: 60,
+  },
+  verifyButton: {
+    fontSize: 16,
+    color: 'white',
+    backgroundColor: 'blue',
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 6,
+    textAlign: 'center',
+    overflow: 'hidden',
+    elevation: 3, // Add elevation for Android shadow
+    shadowColor: '#000', // Add shadow for iOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+  },
+  accountText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 12,
+    marginTop: 10,
+    marginBottom: 5,
+    color: '#333',
+  },
+  accountSection: {
+    marginTop: 5,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    marginBottom: 10,
+    paddingHorizontal: 12,
+    paddingVertical: 10,
+  },
+  accountItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  accountItemText: {
+    fontSize: 16,
+    marginLeft: 7,
+    color: '#333',
+  },
+  logoutContainer: {
+    
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  logoutButton: {
+    backgroundColor: '#FF6347',
+    paddingVertical: 12,
+    paddingHorizontal: 50,
+    borderRadius: 30,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  logoutButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  verifyContainer1: {
+    flexDirection: 'row',
+    paddingLeft: 5,
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingVertical: 10,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    marginBottom: 10,
+  },
+  verifyContainer1: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    paddingVertical: 30,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    elevation: 5,
+  },
+  accountSettingHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  accountText1: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  verifyText1: {
+    fontSize: 16,
+    color: '#666',
+    marginBottom: 20,
+  },
+  verifyButton1: {
+    backgroundColor: '#FF5722',
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    borderRadius: 30,
+    elevation: 3,
+  },
+  verifyButtonText1: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
   },
 });
 
