abc58671e61381615ea3049567b36174ce80a016
harshit-506 <supriyamohapatra658@gmail.com>
2024-03-14 20:16:16 +0530

    done with this

--- homepageDesign/screens/Stacks/OrderScreen.js
+++ homepageDesign/screens/Stacks/OrderScreen.js
@@ -1,4 +1,4 @@
-import { Image, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Image, Pressable, ScrollView, StyleSheet, Text, View,ToastAndroid } from 'react-native'
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigation,useRoute } from '@react-navigation/native';
 import useSWR from "swr"
@@ -38,6 +38,23 @@ const OrderScreen = () => {
         { title: "Payment", content: "Payment Details" },
         { title: "Place Order", content: "Order Summary" },
       ];
+
+      //delete address
+      const handleRemove=async(item)=>{
+        try {
+          const response=await axios.post(`http://192.168.29.163:4000/removeAdd`,{
+            userId:userId,
+            addressId:selectedAddress._id
+          })
+          ToastAndroid.showWithGravity(
+            "Successfully removed, refresh it",
+            ToastAndroid.SHORT,
+            ToastAndroid.BOTTOM
+          );
+        } catch (error) {
+          console.error(error);
+        }
+      }
 
 
     
@@ -87,34 +104,34 @@ const OrderScreen = () => {
         }, 3000);
       };
   return (
-    <ScrollView style={{marginTop:55}}>
-    <View style={{flex:1,paddingHorizontal:20,paddingTop:40}}>
-      <View style={{flexDirection:"row",alignItems:"center",marginBottom:20,justifyContent:"space-between"}}>
+    <ScrollView style={styles.container}>
+    <View style={styles.indexing}>
+      <View style={styles.check}>
          {steps?.map((step,index)=>(
-          <View key={index} style={{justifyContent:"center",alignItems:"center"}}>
+          <View key={index} style={styles.mapping}>
             {
               index > 0 && (
-                <View style={[{flex:1,height:2,backgroundColor:"green"},index<=currentStep && {backgroundColor:"green"}]}/>
+                <View style={[styles.indexcheck,index<=currentStep && {backgroundColor:"green"}]}/>
               )
             }
-            <View style={[{width:30,height:30,borderRadius:15,backgroundColor:"#ccc",justifyContent:"center",alignItems:"center"},index < currentStep && {backgroundColor:"green"}]}>
+            <View style={[styles.currentStep,index < currentStep && {backgroundColor:"green"}]}>
               {index < currentStep ?(
-              <Text style={{fontSize:16,fontWeight:"bold",color:"white"}}>&#10003;</Text>
+              <Text style={styles.currentStepText}>&#10003;</Text>
               ):(
-                <Text style={{fontSize:16,fontWeight:"bold",color:"white"}}>{index+1}</Text>
+                <Text style={styles.currentStepText}>{index+1}</Text>
               )}
             </View>
-            <Text style={{textAlign:"center",marginTop:8}}>{step.title}</Text>
+            <Text style={styles.title}>{step.title}</Text>
           </View>
          ))}
       </View>
     </View>
     {currentStep == 0 && (
       <View style={{marginHorizontal:20}}>
-        <Text style={{fontSize:16,fontWeight:"bold"}}>Select Delivery Address</Text>
+        <Text style={styles.selectDelivery}>Select Delivery Address</Text>
         <Pressable>
            {Object.values(addresses)?.map((item,index)=>(
-            <Pressable style={{borderWidth:1,borderColor:"#D0D0D0",padding:10,flexDirection:"row",alignItems:"center",gap:5,paddingBottom:17,marginVertical:7}} key={index}>
+            <Pressable style={styles.selectDOtCircle} key={index}>
               {/* great logic here */}
               {selectedAddress && selectedAddress._id === item._id ?(
            <FontAwesome5 name="dot-circle" size={24} color="black" />
@@ -124,24 +141,24 @@ const OrderScreen = () => {
               )}
                
                <View style={{marginLeft:6}}>
-               <View style={{flexDirection:"row",alignItems:"center",gap:3}}>
-                    <Text style={{fontSize:15,fontWeight:"bold"}}>{item?.name}</Text>
+               <View style={styles.itemName}>
+                    <Text style={styles.itemNameText}>{item?.name}</Text>
                     <Entypo name="location-pin" size={24} color="redng bh h" />
                   </View>
-                  <Text style={{fontSize:15,color:"#181818"}}>{item?.houseNo},{item?.landmark}</Text>
-                  <Text style={{fontSize:15,color:"#181818"}}>{item?.street}</Text>
-                  <Text style={{fontSize:15,color:"#181818"}}>India,Varanasi</Text>
-                  <Text style={{fontSize:15,color:"#181818"}}>{item?.mobileNo}</Text>
-                  <Text style={{fontSize:15,color:"#181818"}}>{item?.postalCode}</Text>
+                  <Text style={styles.details}>{item?.houseNo},{item?.landmark}</Text>
+                  <Text style={styles.details}>{item?.street}</Text>
+                  <Text style={styles.details}>India,Varanasi</Text>
+                  <Text style={styles.details}>{item?.mobileNo}</Text>
+                  <Text style={styles.details}>{item?.postalCode}</Text>
 
-                  <View style={{flexDirection:"row",alignItems:"center",gap:10,marginTop:7}}>
-                    <Pressable style={{backgroundColor:"#F5F5F5",paddingHorizontal:10,paddingVertical:6,borderRadius:5,borderWidth:0.9,borderColor:"#D0D0D0",alignItems:"center"}}>
+                  <View style={styles.manipulteText}>
+                    <Pressable style={styles.manipulate}>
                            <Text>Edit</Text>
                     </Pressable>
-                    <Pressable style={{backgroundColor:"#F5F5F5",paddingHorizontal:10,paddingVertical:6,borderRadius:5,borderWidth:0.9,borderColor:"#D0D0D0",alignItems:"center"}}>
+                    <Pressable  onPress={()=>handleRemove(selectedAddress)} style={styles.manipulate}>
                            <Text>Remove</Text>
                     </Pressable>
-                    <Pressable style={{backgroundColor:"#F5F5F5",paddingHorizontal:10,paddingVertical:6,borderRadius:5,borderWidth:0.9,borderColor:"#D0D0D0",alignItems:"center"}}>
+                    <Pressable style={styles.manipulate}>
                            <Text>Set as default</Text>
                     </Pressable>
                     
@@ -150,15 +167,8 @@ const OrderScreen = () => {
                      {selectedAddress && selectedAddress?._id === item?._id && (
                       <Pressable 
                        onPress={()=>setCurrentStep(1)}
-                      style={{
-                        backgroundColor:"#008397",
-                        padding:10,
-                        borderRadius:20,
-                        justifyContent:"center",
-                        alignItems:"center",
-                        marginTop:10
-                      }}>
-                        <Text style={{fontSize:15,fontWeight:"bold",color:"white"}}>Deliver to this address</Text>
+                      style={styles.selectAddress}>
+                        <Text style={styles.deliveryText}>Deliver to this address</Text>
                       </Pressable>
                      )}
                   </View>
@@ -171,9 +181,9 @@ const OrderScreen = () => {
 
 {currentStep ==1 && (
   <View style={{marginHorizontal:20}}>
-    <Text style={{fontSize:20,fontWeight:"bold"}}>Choose the delivery options</Text>
+    <Text style={styles.chooseDelivery}>Choose the delivery options</Text>
 
-    <View style={{flexDirection:"row",alignItems:"center",backgroundColor:"white",padding:8,gap:7,borderColor:"#D0D0D0",borderWidth:1,marginTop:10}}>
+    <View style={styles.options}>
       {options ? (
       <FontAwesome5 onPress={()=>setOptions(!options)} name="dot-circle" size={24} color="#008397" />
       ):(
@@ -182,23 +192,23 @@ const OrderScreen = () => {
    
 
     <Text style={{flex:1}}>
-      <Text style={{color:"green",fontWeight:"500"}}>Tommorow by 10pm</Text>{" "}
+      <Text style={styles.tommorow}>Tommorow by 10pm</Text>{" "}
       -Free delivery with your prime membership
     </Text>
     </View>
     <Pressable  
      onPress={()=>setCurrentStep(2)}
-    style={{backgroundColor:"#FFC72C",padding:10,borderRadius:20,justifyContent:"center",alignItems:"center",marginTop:15}}>
-      <Text style={{fontSize:18,fontWeight:"bold"}}>Continue</Text>
+    style={styles.continue}>
+      <Text style={styles.continueText}>Continue</Text>
     </Pressable>
   </View>
 )}
 
 {currentStep ==2 && (
   <View style={{marginHorizontal:20}}>
-    <Text style={{fontSize:20,fontWeight:"bold"}}>Select your payment method</Text>
+    <Text style={styles.chooseDelivery}>Select your payment method</Text>
 
-    <View style={{backgroundColor:"white",padding:8,borderColor:"#D0D0D0",borderWidth:1,flexDirection:"row",alignItems:"center",gap:7,marginTop:12}}>
+    <View style={styles.cash}>
 
   {selectedOptions == "cash" ?(
      <FontAwesome5 name="dot-circle" size={24} color="black" />
@@ -211,7 +221,7 @@ const OrderScreen = () => {
       <Text>Cash on delivery</Text>
     </View>
 
-    <View style={{backgroundColor:"white",padding:8,borderColor:"#D0D0D0",borderWidth:1,flexDirection:"row",alignItems:"center",gap:7,marginTop:12}}>
+    <View style={styles.cash}>
 
       {selectedOptions == "card" ?(
         <FontAwesome5 name="dot-circle" size={24} color="black" />
@@ -226,50 +236,50 @@ const OrderScreen = () => {
     </View>
     <Pressable  
      onPress={()=>setCurrentStep(3)}
-    style={{backgroundColor:"#FFC72C",padding:10,borderRadius:20,justifyContent:"center",alignItems:"center",marginTop:15}}>
-      <Text style={{fontSize:18,fontWeight:"bold"}}>Continue</Text>
+    style={styles.continue}>
+      <Text style={styles.continueText}>Continue</Text>
     </Pressable>
   </View>
 )}
 
 {currentStep == 3 && selectedOptions === "cash" &&(
     <View style={{marginHorizontal:20}}>
-      <Text style={{fontSize:20,fontWeight:"bold"}}>Order Now</Text>
+      <Text style={styles.chooseDelivery}>Order Now</Text>
 
-    <View style={{flexDirection:"row",alignItems:"center",justifyContent:"space-between",gap:8,backgroundColor:"white",padding:8,borderColor:"#D0D0D0",borderWidth:1,marginTop:10}}>
+    <View style={styles.cashOption}>
       <View>
-        <Text style={{fontSize:17,fontWeight:"bold"}}>Save 5% and never ran out</Text>
-        <Text style={{fontSize:15,color:"gray",marginTop:5}}>Turn on auto deliveries</Text>
+        <Text style={styles.save}>Save 5% and never ran out</Text>
+        <Text style={styles.turnOn}>Turn on auto deliveries</Text>
       </View>
       <MaterialIcons name='keyboard-arrow-right' size={24} color={"black"} />
     </View>
  
-    <View style={{backgroundColor:"white",padding:8,borderColor:"#D0D0D0",borderWidth:1,marginTop:10}}>
+    <View style={styles.cashing}>
         <Text>Shipping to {selectedAddress?.name}</Text>
-        <View style={{flexDirection:"row",alignItems:"center",justifyContent:"space-between"}}>
+        <View style={styles.orderItem}>
           <Text style={{fontSize:16,fontWeight:"500",color:"gray"}}>Items</Text>
           <Text style={{color:"gray",fontSize:16}}>&#8377;{total}</Text>
         </View>
 
-        <View style={{flexDirection:"row",alignItems:"center",justifyContent:"space-between"}}>
-          <Text style={{fontSize:16,fontWeight:"500",color:"gray"}}>Delivery</Text>
-          <Text style={{color:"gray",fontSize:16}}>0</Text>
+        <View style={styles.orderItem}>
+          <Text style={styles.itemColor}>Delivery</Text>
+          <Text style={styles.text0}>0</Text>
         </View>
 
-        <View style={{flexDirection:"row",alignItems:"center",justifyContent:"space-between"}}>
-          <Text style={{fontSize:18,fontWeight:"bold"}}>Order Total</Text>
+        <View style={styles.orderItem}>
+          <Text style={styles.continueText}>Order Total</Text>
           <Text style={{color:"#C60C30",fontSize:16}}>&#8377;{total}</Text>
         </View>
     </View>
 
-    <View style={{backgroundColor:"white",padding:8,borderColor:"#D0D0D0",borderWidth:1,marginTop:10}}>
-      <Text style={{fontSize:16,color:"gray"}}>Pay with</Text>
-      <Text style={{fontSize:16,fontWeight:"600",marginTop:7}}>Pay on delivery Cash</Text>
+    <View style={styles.cashing}>
+      <Text style={styles.payWith}>Pay with</Text>
+      <Text style={styles.payOn}>Pay on delivery Cash</Text>
     </View>
     <Pressable 
     onPress={handleOrder}
-    style={{backgroundColor:"#FFC72C",padding:10,borderRadius:20,justifyContent:"center",alignItems:"center",marginTop:20}}>
-      <Text style={{fontSize:16,fontWeight:"bold"}}>Place your order</Text>
+    style={styles.placeOrder}>
+      <Text style={styles.placeYour}>Place your order</Text>
     </Pressable>
     </View>
 )}
@@ -312,6 +322,7 @@ export default OrderScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    
   },
   image1: {
     width: "100%",
@@ -366,4 +377,108 @@ buttonText: {
   fontSize: 18,
   fontWeight: 'bold',
 },
+indexing:{
+  flex:1,paddingHorizontal:20,paddingTop:40,
+},
+check:{
+  flexDirection:"row",alignItems:"center",marginBottom:20,justifyContent:"space-between",
+},
+mapping:{
+  justifyContent:"center",alignItems:"center",
+},
+indexcheck:{
+  flex:1,height:2,backgroundColor:"green"
+},
+currentStep:{
+  width:30,height:30,borderRadius:15,backgroundColor:"#ccc",justifyContent:"center",alignItems:"center",
+},
+currentStepText:{
+  fontSize:16,fontWeight:"bold",color:"white",
+},
+title:{
+  textAlign:"center",marginTop:8,
+},
+selectDelivery:{
+  fontSize:16,fontWeight:"bold",
+},
+selectDOtCircle:{
+  borderWidth:1,borderColor:"#D0D0D0",padding:10,flexDirection:"row",alignItems:"center",gap:5,paddingBottom:17,marginVertical:7,
+},
+details:{
+  fontSize:15,color:"#181818",
+},
+manipulate:{
+  backgroundColor:"#F5F5F5",paddingHorizontal:10,paddingVertical:6,borderRadius:5,borderWidth:0.9,borderColor:"#D0D0D0",alignItems:"center",
+},
+selectAddress:{
+  backgroundColor:"#008397",
+  padding:10,
+  borderRadius:20,
+  justifyContent:"center",
+  alignItems:"center",
+  marginTop:10,
+},
+options:{
+  flexDirection:"row",alignItems:"center",backgroundColor:"white",padding:8,gap:7,borderColor:"#D0D0D0",borderWidth:1,marginTop:10,
+},
+continue:{
+  backgroundColor:"#FFC72C",padding:10,borderRadius:20,justifyContent:"center",alignItems:"center",marginTop:15,
+},
+cash:{
+  backgroundColor:"white",padding:8,borderColor:"#D0D0D0",borderWidth:1,flexDirection:"row",alignItems:"center",gap:7,marginTop:12,
+},
+cashOption:{
+  flexDirection:"row",alignItems:"center",justifyContent:"space-between",gap:8,backgroundColor:"white",padding:8,borderColor:"#D0D0D0",borderWidth:1,marginTop:10,
+},
+cashing:{
+  backgroundColor:"white",padding:8,borderColor:"#D0D0D0",borderWidth:1,marginTop:10,
+},
+orderItem:{
+  flexDirection:"row",alignItems:"center",justifyContent:"space-between",
+},
+itemColor:{
+  fontSize:16,fontWeight:"500",color:"gray",
+},
+placeOrder:{
+  backgroundColor:"#FFC72C",padding:10,borderRadius:20,justifyContent:"center",alignItems:"center",marginTop:20,
+},
+itemName:{
+  flexDirection:"row",alignItems:"center",gap:3,
+},
+itemNameText:{
+  fontSize:15,fontWeight:"bold",
+},
+manipulteText:{
+  flexDirection:"row",alignItems:"center",gap:10,marginTop:7,
+},
+deliveryText:{
+  fontSize:15,fontWeight:"bold",color:"white",
+},
+chooseDelivery:{
+  fontSize:20,fontWeight:"bold",
+},
+tommorow:{
+  color:"green",fontWeight:"500",
+},
+continueText:{
+  fontSize:18,fontWeight:"bold",
+},
+save:{
+  fontSize:17,fontWeight:"bold",
+},
+turnOn:{
+  fontSize:15,color:"gray",marginTop:5,
+},
+text0:{
+  color:"gray",fontSize:16,
+},
+payWith:{
+  fontSize:16,color:"gray",
+},
+payOn:{
+  fontSize:16,fontWeight:"600",marginTop:7,
+},
+placeYour:{
+  fontSize:16,fontWeight:"bold",
+}
 });
